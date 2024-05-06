908039c124888efc9df6f4d1ecdb3ab304948b31
Mark Satin <msatin2009@gmail.com>
2024-05-06 14:33:46 -0400

    init commit

---
+++ app/ui/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="w-full h-fit flex justify-center items-center bg-white mt-8">
+      <div className="w-3/4 h-full">
+        <div>word</div>
+        <div>word</div>
+        <div>word</div>
+      </div>
+    </footer>
+  );
+}
