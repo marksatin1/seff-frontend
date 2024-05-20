e9d3692f93723179b51fae3451cf789d69aced09
Mark Satin <msatin2009@gmail.com>
2024-05-20 10:23:01 -0400

    init commit

---
+++ app/lib/data/dummy-search-results.ts
@@ -0,0 +1,53 @@
+export const newSearchResults = [
+  {
+    brandLogoPath: "/logos/chrono.svg",
+    authScore: 55,
+    price: 3500,
+    layawayLogoPath: "/logos/klarna.svg",
+  },
+  {
+    brandLogoPath: "/logos/neiman.svg",
+    authScore: 100,
+    price: 3500,
+    layawayLogoPath: "/logos/paypal.svg",
+  },
+  {
+    brandLogoPath: "/logos/bergdorf.svg",
+    authScore: 100,
+    price: 4500,
+    layawayLogoPath: "/logos/afterpay.svg",
+  },
+  {
+    brandLogoPath: "/logos/harrods.svg",
+    authScore: 68,
+    price: 10000,
+    layawayLogoPath: "",
+  },
+];
+
+export const preOwnedSearchResults = [
+  {
+    brandLogoPath: "/logos/ebay.svg",
+    authScore: 55,
+    hits: "5 hits",
+    layawayLogoPath: "/logos/paypal.svg",
+    priceLow: 3500,
+    priceHigh: 15000,
+  },
+  {
+    brandLogoPath: "/logos/therealreal.svg",
+    authScore: 100,
+    hits: "3 hits",
+    layawayLogoPath: "/logos/klarna.svg",
+    priceLow: 4000,
+    priceHigh: 15000,
+  },
+  {
+    brandLogoPath: "/logos/fbmarketplace.svg",
+    authScore: 0,
+    hits: "3 hits (locally)",
+    layawayLogoPath: "",
+    priceLow: 6000,
+    priceHigh: 15000,
+  },
+];
