c883e629da295687cb87e4cb95bb7aa5a783149d
Mark Satin <msatin2009@gmail.com>
2024-05-20 10:15:56 -0400

    add PropTypes for NewSearchResult and PreOwnedSearchResult

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,9 +1,29 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
 export type Filter = {
   name: string;
   options: string[];
-}
+};
 
 export type Sort = {
   name: string;
   options: string[];
-}
+};
+
+export type NewSearchResultPropTypes = {
+  brandLogoPath: string;
+  authScore: number;
+  price: number;
+  offer?: string;
+  promotion?: string;
+  layawayLogoPath: string;
+};
+
+export type PreOwnedSearchResultPropTypes = {
+  brandLogoPath: string;
+  authScore: number;
+  hits: string;
+  priceLow: number;
+  priceHigh: number;
+  layawayLogoPath: string;
+};
