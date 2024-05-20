78f89707e9d4dcebe37b0338eb8391903b720f55
Mark Satin <msatin2009@gmail.com>
2024-05-20 10:33:27 -0400

    wrap logo in Link

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-3 bg-white">
-      <Image
-        src="/icons/seff-primary.svg"
-        width={249}
-        height={167}
-        alt="SEFF logo in a light purple color"
-        priority
-        className="w-24 h-auto"
-      />
+    <header className="flex justify-between items-center p-3 mb-2 bg-white">
+      <Link href="/">
+        <Image
+          src="/icons/seff-primary.svg"
+          width={249}
+          height={167}
+          alt="SEFF logo in a light purple color"
+          priority
+          className="w-24 h-auto"
+        />
+      </Link>
       <button
         type="button"
         className="font-light text-sm underline text-gray border border-secondary rounded-lg p-2 shadow-md"
