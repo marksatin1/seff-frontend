529c4470af9acc8c162604fa145f6aa9f0da5147
Mark Satin <msatin2009@gmail.com>
2024-05-20 10:33:20 -0400

    add CTA, Search to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Navbar from "./ui/navbar";
+import Search from "./ui/search";
 
 export default function Home() {
   return (
-    <main className="w-full h-full">
-      <Navbar />
-    </main>
+    <section className="h-full flex flex-col text-center text-xl mx-4">
+      <h1 className="my-16">
+        <span className="block">Search for luxury</span>
+        <span className="block">
+          <span className="font-bold text-primary">fashion</span> and{" "}
+          <span className="font-bold text-secondary-light">jewelry</span>
+        </span>
+        <span className="block font-bold text-2xl leading-8 tracking-widest">ALL OVER</span>
+        <span className="block">the world!</span>
+      </h1>
+      <Search />
+    </section>
   );
 }
