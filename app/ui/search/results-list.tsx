01db69f3bbcc6cb1d2421050d537f36b63545687
Mark Satin <msatin2009@gmail.com>
2024-05-25 14:06:00 -0400

    init commit

---
+++ app/ui/search/results-list.tsx
@@ -0,0 +1,19 @@
+import { NewSearchResultPropTypes } from "@/app/lib/definitions";
+import { newSearchResults } from "@/app/lib/data/dummy-search-results";
+import { filterResultsList } from "@/app/lib/utils";
+
+export default function ResultsList({ query }: { query: string }) {
+  const filteredResults = filterResultsList(newSearchResults, query);
+
+  return (
+    <div className="border-2 border-primary-dark w-full h-full flex flex-col gap-4 mt-3 p-6 rounded-b-md shadow-lg">
+      {filteredResults?.map((res: NewSearchResultPropTypes, idx: number) => {
+        return (
+          <div key={`${res}-${idx}`}>
+            <p>{res.brandLogoPath}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ app/ui/search/search-bar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export default function Search() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(searchTerm: string) {
+    const params = new URLSearchParams(searchParams);
+    
+    if (searchTerm) {
+      params.set("query", searchTerm);
+    } else {
+      params.delete("query");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }
+
+  return (
+    <form className="shadow-md">
+      <div className="border-2 border-neutral-dark rounded-md px-4 pt-2 pb-4 bg-white">
+        <div className="flex items-center">
+          <Image src="/icons/search.svg" width={24} height={24} alt="Magnifying glass icon" />
+          <input
+            type="text"
+            name="search"
+            id="search-bar"
+            placeholder="Browse..."
+            defaultValue={searchParams.get("query")?.toString()}
+            onChange={(e: any) => handleSearch(e.target.value)}
+            className="outline-none rounded-sm p-1 pl-2 border-b border-primary w-full "
+          />
+        </div>
+      </div>
+    </form>
+  );
+}
